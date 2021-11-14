65871696d23247731ba49244506edb7a7af822ae
Vladimir Fibe <motiw@me.com>
2021-11-14 22:34:51 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Vladimir on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
