d4be3e1f9625f6e45a88c3d93f3fff1d8c792985
Vladimir Fibe <motiw@me.com>
2021-11-16 17:32:19 +0600

    OnboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,17 +12,86 @@ struct OnboardingView: View {
   @AppStorage("onboarding") var isOnboardingViewActive: Bool?
   
   // MARK: - BODY
-    var body: some View {
+  var body: some View {
+    ZStack {
+      Color("ColorBlue").ignoresSafeArea()
       VStack(spacing: 20.0) {
-        Text("Onboarding").font(.largeTitle)
-        Button {
-          isOnboardingViewActive = false
-        } label: {
-          Text("Start")
-        }
-
+        
+        // MARK: - Header
+        Spacer()
+        
+        VStack(spacing: 0) {
+          Text("Share.")
+            .font(.system(size: 60))
+            .fontWeight(.heavy)
+          Text("""
+            It's not how much we give but
+            how much love we put into giving.
+            """)
+            .font(.title3)
+            .fontWeight(.light)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 10)
+        } // Header
+        .foregroundColor(.white)
+        // MARK: - Center
+        ZStack {
+          ZStack {
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 40)
+              .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+              .stroke(.white.opacity(0.2), lineWidth: 80)
+              .frame(width: 260, height: 260, alignment: .center)
+            Image("character-1")
+              .resizable()
+              .scaledToFit()
+          } // ZStack
+        } // Center
+        Spacer()
+        // MARK: - Footer
+        
+        ZStack {
+          Capsule()
+            .fill(.white.opacity(0.2))
+          Capsule()
+            .fill(.white.opacity(0.2))
+            .padding(8)
+          
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+          HStack {
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: 80)
+            Spacer()
+          }
+          HStack {
+            ZStack {
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }
+            .foregroundColor(.white)
+            .frame(width: 80, height: 80, alignment: .center)
+            .onTapGesture {
+              isOnboardingViewActive = false
+            }
+            Spacer()
+          }
+        } // Footer
+        .frame(height: 80, alignment: .center)
+        .padding()
       } // VStack
-    }
+    } // ZStack
+  }
 }
 // MARK: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
